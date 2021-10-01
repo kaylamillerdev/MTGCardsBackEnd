874141aba40fe9839cce9f585b4cd02b8b99ef77
Kayla Miller <kayla@kaylamiller.dev>
2021-10-01 10:59:57 -0400

    Add cors headers code to fix current cors error in production site.
    Signed-off-by: Kayla Miller <kayla@kaylamiller.dev>

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ var logger = require('morgan');
 var models = require('./models');
 var passport = require('passport');
 var session = require('express-session');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors( {origin: ['http://localhost:4200', 'https://relaxed-lalande-20b424.netlify.app']}));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://https://relaxed-lalande-20b424.netlify.app/');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
